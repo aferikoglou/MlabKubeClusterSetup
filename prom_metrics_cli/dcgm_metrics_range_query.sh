1d01a95bf839b79701403a6b2382ba83a4faa540
dimitris <dimitrisg@moveo.ai>
2022-04-19 19:59:28 +0300

    chore: adding output filename option in plot cli

--- prom_metrics_cli/dcgm_metrics_range_query.sh
+++ prom_metrics_cli/dcgm_metrics_range_query.sh
@@ -7,12 +7,23 @@ while [[ $# -gt 0 ]]; do
       shift
       shift
       ;;
+    -o|--output)
+      OUT="$2"
+      shift
+      shift
+      ;;
   esac
 done
 
 if [ -z "$INTERVAL" ]
 then
   INTERVAL="30"
+fi
+
+if [ -z "$OUT" ]
+then
+  echo "(-o | --output) argument can not be empty"
+  exit 0
 fi
 
 STEP="$(($INTERVAL / 100))"
@@ -23,42 +34,43 @@ fi
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-echo $parent_path
-
 cd "$parent_path"
 
-go run main.go -p 'api/v1/query_range' -i $INTERVAL -params `{"step": "1s", "query": "DCGM_FI_DEV_DEC_UTIL"}` &>> log.txt
-tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)"
+# go run main.go -p 'api/v1/query_range' -i $INTERVAL -params `{"step": "1s", "query": "DCGM_FI_DEV_DEC_UTIL"}` &>> log.txt
+# tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)" -o $OUT
 
-go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_ENC_UTIL"}' &>> log.txt
-tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)"
+# go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_ENC_UTIL"}' &>> log.txt
+# tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)" -o $OUT
 
-go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_FB_FREE"}' &>> log.txt
-tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)"
+# go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_FB_FREE"}' &>> log.txt
+# tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)" -o $OUT
 
-go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_FB_USED"}' &>> log.txt
-tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)"
+# go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_FB_USED"}' &>> log.txt
+# tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)" -o $OUT
 
-go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_GPU_TEMP"}' &>> log.txt
-tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)"
+# go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_GPU_TEMP"}' &>> log.txt
+# tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)" -o $OUT
 
-go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_VGPU_LICENSE_STATUS"}' &>> log.txt
-tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)"
+# go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_VGPU_LICENSE_STATUS"}' &>> log.txt
+# tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)" -o $OUT
 
-go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_MEM_CLOCK"}' &>> log.txt
-tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)"
+# go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_MEM_CLOCK"}' &>> log.txt
+# tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)" -o $OUT
 
-go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_MEM_COPY_UTIL"}' &>> log.txt
-tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)"
+# go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_MEM_COPY_UTIL"}' &>> log.txt
+# tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)" -o $OUT
 
-go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_NVLINK_BANDWIDTH_TOTAL"}' &>> log.txt
-tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)"
+# go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_NVLINK_BANDWIDTH_TOTAL"}' &>> log.txt
+# tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)" -o $OUT
 
-go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_PCIE_REPLAY_COUNTER"}' &>> log.txt
-tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)"
+# go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_PCIE_REPLAY_COUNTER"}' &>> log.txt
+# tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)" -o $OUT
 
-go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_SM_CLOCK"}' &>> log.txt
-tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)"
+# go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_SM_CLOCK"}' &>> log.txt
+# tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)" -o $OUT
 
-go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_XID_ERRORS"}' &>> log.txt
-tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)"
+# go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": "DCGM_FI_DEV_XID_ERRORS"}' &>> log.txt
+# tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)" -o $OUT
+
+go run main.go -p 'api/v1/query_range' -i $INTERVAL -params '{"step": "1s", "query": ":node_memory_MemAvailable_bytes:sum"}' &>> log.txt
+tail -1 log.txt | python plot/plot.py -f "__name__" -x "Time(s)" -o $OUT
--- prom_metrics_cli/plot/plot.py
+++ prom_metrics_cli/plot/plot.py
@@ -1,8 +1,32 @@
 from matplotlib import container
 import matplotlib.pyplot as plt
-import json 
+import json
 import os
 import argparse
+import re
+import sys
+
+def validate_filename(filename):
+    pattern  = r'^[a-z0-9][a-z0-9_-]+[a-z0-9]$'
+    pat = re.compile(pattern)
+    return re.fullmatch(pat, filename)
+
+def find_max_id(dirname: str, outfile: str)-> int:
+    max_id = 0
+    for filename in os.listdir(dirname + "/figures"):
+        tmp = filename.split(".")
+        if (not len(tmp) > 1) or (not "_".join(tmp[0].split("_")[:-1]).startswith(outfile)):
+            continue
+        try:
+            tmp = int(tmp[0].split("_")[-1])
+        except:
+            continue
+
+        if tmp > max_id:
+            max_id = tmp
+
+    return max_id + 1
+
 
 parser = argparse.ArgumentParser(description='Parse matplotlib labels.')
 parser.add_argument('-x', action='store', type=str,
@@ -11,8 +35,14 @@ parser.add_argument('-y', action='store', type=str,
                     help='ylabel')
 parser.add_argument('-l', '--legend-list', nargs='+', default=[], help="Manually import a list of legends for matplotlib")
 parser.add_argument('-f', action='store', help="Json field from which to extract legends")
+parser.add_argument('-o', action='store', required = True,  help="Name of the output png. Name can only contain - _ \
+    or alphanumerics and can't start or end with - or _")
 
 args = parser.parse_args()
+
+if not validate_filename(args.o):
+    parser.print_help()
+    sys.exit(1)
 
 dirname, _ = os.path.split(os.path.abspath(__file__))
 
@@ -26,7 +56,7 @@ try:
 except:
     print("Input data not json serializable")
 
-legend = []    
+legend = []
 for result in data["data"]["result"]:
 
     base = float(result["values"][0][0])
@@ -35,7 +65,7 @@ for result in data["data"]["result"]:
     for v in result["values"]:
         time.append(float(v[0]) - base)
         position.append(float(v[1]))
-            
+
     plt.plot(time, position)
     plt.xlabel(args.x)
     plt.ylabel(args.y)
@@ -44,16 +74,8 @@ for result in data["data"]["result"]:
 
     if not os.path.exists(dirname + "/figures"):
         os.makedirs(dirname + "/figures")
-    
-    max_id = 0
-    for filename in os.listdir(dirname + "/figures"):
-        try:
-            tmp = int(filename.split(".")[0])
-        except:
-            continue
 
-        if tmp > max_id:
-            max_id = tmp
+max_id = find_max_id(dirname, args.o)
 
 if len(args.legend_list) > 0:
     legend = []
@@ -63,4 +85,4 @@ if len(args.legend_list) > 0:
 elif args.f is not None:
     plt.legend(legend)
 
-plt.savefig(dirname + '/figures/' + str(max_id + 1) + '.png')
+plt.savefig(dirname + '/figures/' + args.o + "_" + str(max_id) + '.png')
