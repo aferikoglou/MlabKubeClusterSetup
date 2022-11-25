21b7fb5129cc9a4818709569025cdf0286718aa5
dimitris <dimgatz98@gmail.com>
2022-11-25 18:42:20 +0200

    fix: use clf instead of clear to delete figures

--- prom_metrics_cli/plot/plot.py
+++ prom_metrics_cli/plot/plot.py
@@ -225,7 +225,7 @@ for i, result in enumerate(data["data"]["result"]):
         plt.legend()
         plt.tight_layout()
         plt.savefig(os.path.join(filepath, args.o + "_" + str(max_id) + '.png'))
-        plt.figure().clear()    
+        plt.clf()
     
 
 if args.total and lines > 0:
@@ -252,4 +252,4 @@ if args.total and lines > 0:
     plt.legend()
     plt.tight_layout()
     plt.savefig(os.path.join(filepath, args.o + "_" + str(max_id) + '.png',))
-    
+    plt.clf()
